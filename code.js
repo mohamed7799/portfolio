03dba42c5eb07b4e52fc5870bf1031558f584948
mohmed <mohmedamged9@gmail.com>
2021-09-25 12:21:01 +0200

    first commit

---
+++ code.js
@@ -0,0 +1,7 @@
+"use strict"
+
+// functions
+let check = (item, _class) => item.classList.contains(_class);
+let add = (item, _class) => item.classList.add(_class);
+let remove = (item, _class) => item.classList.remove(_class);
+let toggle = (item, _class) => check(item, _class) ? remove(item, _class) : add(item, _class);
