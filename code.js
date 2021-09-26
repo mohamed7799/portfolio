3b75d9f5216d951add5cd57d082275f9bb3ed7c9
mohmed <mohmedamged9@gmail.com>
2021-09-26 12:19:36 +0200

    push repo

--- code.js
+++ code.js
@@ -1,7 +1,27 @@
-"use strict"
+"use strict";
 
 // functions
-let check = (item, _class) => item.classList.contains(_class);
+
 let add = (item, _class) => item.classList.add(_class);
 let remove = (item, _class) => item.classList.remove(_class);
-let toggle = (item, _class) => check(item, _class) ? remove(item, _class) : add(item, _class);
+
+//dom variables
+const menuBars = document.querySelector(".menu");
+const dropDownMenu = document.querySelector(".dropDown-menu");
+const closeDropDownMenu = document.querySelector(".close-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+//events
+
+menuBars.addEventListener("click", () => {
+  remove(dropDownMenu, "hidden");
+});
+
+closeDropDownMenu.addEventListener("click", () => {
+  add(dropDownMenu, "hidden");
+});
+
+mobileMenu.addEventListener("click", (e) => {
+  if (e.target.parentElement.parentElement === mobileMenu) {
+    add(dropDownMenu, "hidden");
+  }
+});
